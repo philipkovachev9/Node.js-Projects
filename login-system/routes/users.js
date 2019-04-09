24c1ab20eac9b5452671984600df2f9334a3ca40
Philip Kovachev <pkova@softserveinc.com>
2019-04-09 11:54:31 +0300

    Added validation to the register form

--- login-system/routes/users.js
+++ login-system/routes/users.js
@@ -7,4 +7,35 @@ router.get('/login', (req, res) => res.render('login'));
 //Register Page
 router.get('/register', (req, res) => res.render('register'));
 
+// Register Handle
+router.post('/register', (req, res) => {
+ const { name, email, password, password2 } = req.body;
+ let errors = [];
+
+ // Check for blank required fields
+ if(!name || !email || !password || !password2) {
+   errors.push({ msg: 'All fields must be filled' });
+ }
+
+ // Check if passwords match
+ if(password !== password2) {
+  errors.push({ msg: 'Passwords do not match' });
+ }
+
+ // Check password length
+ if(password.length < 6) {
+  errors.push({ msg: 'Password should be at least six characters' })
+ }
+  if(errors.length > 0) {
+   res.render('register', {
+   errors,
+   name,
+   email,
+   password,
+   password2
+   });
+  } else {
+    res.send('pass');
+  }
+});
 module.exports = router;
