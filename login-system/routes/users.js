f10cb38de08184c3fe0e20ee9044ec7c8de3f3da
Philip Kovachev <pkova@softserveinc.com>
2019-04-10 16:14:57 +0300

    Added email quiering in Mongo, so that a user can't register with the same email

--- login-system/routes/users.js
+++ login-system/routes/users.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+// User model
+const User = require('../models/User');
 
 //Login Page
 router.get('/login', (req, res) => res.render('login'));
@@ -35,7 +39,27 @@ router.post('/register', (req, res) => {
    password2
    });
   } else {
-    res.send('pass');
+    // Validation Pass
+    User.findOne({ email: email })
+    .then(user => {
+      if(user) {
+        //User exists
+        errors.push({msg: 'Email is already registered'});
+        res.render('register', {
+          errors,
+          name,
+          email,
+          password,
+          password2
+          });
+      } else {
+        const newUser = new User({
+          name,
+          email,
+          password
+        });
+      }
+    });
   }
 });
 module.exports = router;
